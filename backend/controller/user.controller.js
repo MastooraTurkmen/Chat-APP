5fbebda73e00d99dd7178580c6cefa5ed1391076
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-19 17:10:17 +0430

    select password

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -3,7 +3,7 @@ import User from "../model/user.model.js";
 export const getUserForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
-        const allUsers = await User.find({ _id: { $ne: loggedInUserId } })
+        const allUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
 
         res.status(200).json(allUsers)
     } catch (error) {
