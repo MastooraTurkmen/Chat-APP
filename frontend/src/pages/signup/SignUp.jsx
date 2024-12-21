102e50d347c4df8a599950ae7c91ca912b2f34f5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-21 20:10:44 +0430

    signup

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -94,7 +94,9 @@ const SignUp = () => {
             Already have an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2">Sing Up</button>
+            <button className="btn btn-block btn-sm mt-2">
+              {loading ? <span className="loading loading-spinner"></span> : "Sing Up"}
+            </button>
           </div>
         </form>
       </div>
