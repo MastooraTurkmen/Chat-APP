d7151d47ae143e93f6cfc6d5632805e7fa1a4b91
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-21 20:10:35 +0430

    use signup

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import toast from "react-hot-toast"
+import { useAuthContext } from "../context/AuthContext"
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false)
+    const { authUser, setAuthUser } = useAuthContext()
 
     const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
         const success = handleInputError({ fullName, username, password, confirmPassword, gender })
@@ -18,6 +20,13 @@ const useSignup = () => {
             })
 
             const data = await res.json()
+
+            if (data.error) {
+                throw new Error(data.error)
+            }
+
+            localStorage.setItem("chat-user", JSON.stringify(data))
+            setAuthUser(data)
             console.log(data);
         } catch (error) {
             toast.error(error.message)
